371e11b8be36d05defaeed97aa2b2f7e3b3322b2
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-04-25 13:56:57 +0100

    Added setup.py and read.py, decoding in read.py is weird.

---
+++ read.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+import serial
+import time
+
+
+
+ser = serial.Serial('/dev/ttyUSB0',115200,timeout=1)
+#ser = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
+
+def send_req(str):
+  print("Send:" + str);  
+  ser.write(str.encode('utf-8'))
+  
+
+def show_res():
+  while(1):
+    line=ser.readline()
+    if (line):
+      try:  
+        print(line.decode())
+      except:
+        continue;  
+    else:
+      break;
+
+line = ser.readline()
+
+while(True):
+    show_res()
+    time.sleep(1)
+
+
+ser.close()
