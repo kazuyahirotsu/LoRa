30831f8ebf1c1fd401ffb1597e325bd881cb3029
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-06-21 15:34:53 +0900

    fit curve with distance plot

--- project/GPS-visualization-Python/rssi_plot_scipy.py
+++ project/GPS-visualization-Python/rssi_plot_scipy.py
@@ -3,9 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-data_path='data.csv'
-data = pd.read_csv(data_path, names=['RSSI','LATITUDE', 'LONGITUDE'], sep=',')
-x=data[['LONGITUDE']]
+# lon0, lat0 = 35.650798, 139.779477
+lon0, lat0 = 38.721527, 141.092104
+r = 6378.137*1000
+data_path='data_6_16_2.csv'
+data = pd.read_csv(data_path, names=['RSSI','LONGITUDE', 'LATITUDE'], sep=',')
+
+data['DISTANCE'] = r*np.arccos(np.sin(lat0*np.pi/180)*np.sin(data['LATITUDE']*np.pi/180)+np.cos(lat0*np.pi/180)*np.cos(data['LATITUDE']*np.pi/180)*np.cos(lon0*np.pi/180-data['LONGITUDE']*np.pi/180))
+
+x=data[['DISTANCE']]
 y=data[['RSSI']]
 
 def func(x, a, b):
@@ -14,17 +20,16 @@ def func(x, a, b):
 x = x.to_numpy()[:, 0]
 y = y.to_numpy()[:, 0]
 
-# a, b = 1.2, 2
-# x = np.linspace(0.001, 10, 100)
-# y = func(x, a, b) + np.random.normal(0, 0.5, len(x))
-
 popt, pcov = curve_fit(func,x,y) 
-print(popt)
+print("y="+str(popt[0])+str(popt[1])+"*log(x)")
 y_pred = func(x, popt[0], popt[1])
 plt.plot(x,y_pred,color="red")
 
 plt.scatter(x,y)
-plt.xlabel("Longitude")
+plt.xlabel("Distance from Tx(m)")
 plt.ylabel("RSSI(dBm)")
 plt.gca().get_xaxis().get_major_formatter().set_useOffset(False)
-plt.legend(["fit curve","measured value","linear regression of measured value"])
+# plt.legend(["simulation","measured value","linear regression of measured value"])
+plt.legend(["fit curve","linear regression of measured value"])
+print("-122dBm at "+str(int(np.exp((-122+57.035530363980186)/(-9.009988560782537))))+"m")
+print("-132dBm at "+str(int(np.exp((-132+57.035530363980186)/(-9.009988560782537))))+"m")
