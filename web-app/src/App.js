2bd7d80207d3cb99e989c97c702e77aef4538427
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-11-30 17:43:22 +0900

    Made charts look better, added footer

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -16,7 +16,6 @@ function App() {
     data_0003DO_2:[],
     data_0003TEMP:[],
   })
-  const [haveData, setHaveData] = useState(false)
   const options = [
     {value: '24h', text: '24h'},
     {value: '12h', text: '12h'},
@@ -27,9 +26,7 @@ function App() {
   const [selectedTimerange, setSelectedTimerange] = useState(options[0].value);
   const fetchData = async (measure_name,timerange) => {
       const q = `SELECT * FROM izunuma.izunuma WHERE (measure_name='${measure_name}') AND time >= ago(${timerange}) ORDER BY time ASC`
-      console.log(q)
       const res = await api.rawQuery(q);
-      console.log(res);
       const chartData = res.Rows.map((d)=>({
         time: moment(d.Data[2].ScalarValue.slice(0, -10),"YYYY-MM-DD HH:mm:ss").valueOf(),
         value: d.Data[3].ScalarValue,
@@ -39,8 +36,6 @@ function App() {
 
   useEffect(() => {
     const initiallize = async () => {
-      console.log(process.env.REACT_APP_AWS_ID);
-      
       await api.setConfiguration('ap-northeast-1', process.env.REACT_APP_AWS_ID, process.env.REACT_APP_AWS_SECRET)
     }
     initiallize()
@@ -69,7 +64,6 @@ function App() {
 
 
   const handleSelectChange = e => {
-    console.log(e.target.value);
     setSelectedTimerange(e.target.value);
   };
 
@@ -121,12 +115,13 @@ function App() {
             <p>DO</p>
             <ResponsiveContainer width="100%" height={400}>
               <LineChart width={1000} height={400} className="mx-auto">
-                <Line type="monotone" dataKey="value" data={data.data_0001DO_1} stroke="#8884d8"/>
-                <Line type="monotone" dataKey="value" data={data.data_0001DO_2} stroke="#82ca9d"/>
+                <Line type="monotone" dataKey="value" data={data.data_0001DO_1} name="DO1" stroke="#8884d8" dot={false}/>
+                <Line type="monotone" dataKey="value" data={data.data_0001DO_2} name="DO2" stroke="#82ca9d" dot={false}/>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" type="number" tickFormatter={(unixTimestamp) => moment(unixTimestamp).format("YYYY-MM-DD HH:mm:ss")} domain={['auto', 'auto']} scale="time"  allowDuplicatedCategory={false} angle={-45} textAnchor="end" tick={{ fontSize: '.7rem', color: 'white'}} height={100} className="text-white"/>
                 <YAxis type="number" domain={[0,20]}/>
                 <Tooltip labelFormatter={(label) => moment(label).format("YYYY-MM-DD HH:mm:ss")}/>
+                <Legend />
               </LineChart>
             </ResponsiveContainer>
             {data.data_0001DO_1.length==0?
@@ -148,11 +143,12 @@ function App() {
             <p>TEMP</p>
             <ResponsiveContainer width="100%" height={400}>
               <LineChart width={1000} height={400} data={data.data_0001TEMP} className="mx-auto">
-                <Line type="monotone" dataKey="value"  stroke="#8884d8"/>
+                <Line type="monotone" dataKey="value" name="TEMP" stroke="#ffc658" dot={false}/>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" type="number" tickFormatter={(unixTimestamp) => moment(unixTimestamp).format("YYYY-MM-DD HH:mm:ss")} domain={['auto', 'auto']} scale="time"  allowDuplicatedCategory={false} angle={-45} textAnchor="end" tick={{ fontSize: '.7rem', color: 'white'}} height={100} className="text-white"/>
                 <YAxis type="number" domain={[0,20]}/>
                 <Tooltip labelFormatter={(label) => moment(label).format("YYYY-MM-DD HH:mm:ss")}/>
+                <Legend />
               </LineChart>
             </ResponsiveContainer>
             {data.data_0001TEMP.length==0?
@@ -172,12 +168,13 @@ function App() {
             <p>DO</p>
             <ResponsiveContainer width="100%" height={400}>
               <LineChart width={1000} height={400} className="mx-auto">
-                <Line type="monotone" dataKey="value" data={data.data_0002DO_1} stroke="#8884d8"/>
-                <Line type="monotone" dataKey="value" data={data.data_0002DO_2} stroke="#82ca9d"/>
+                <Line type="monotone" dataKey="value" data={data.data_0002DO_1} name="DO1" stroke="#8884d8" dot={false}/>
+                <Line type="monotone" dataKey="value" data={data.data_0002DO_2} name="DO2" stroke="#82ca9d" dot={false}/>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" type="number" tickFormatter={(unixTimestamp) => moment(unixTimestamp).format("YYYY-MM-DD HH:mm:ss")} domain={['auto', 'auto']} scale="time"  allowDuplicatedCategory={false} angle={-45} textAnchor="end" tick={{ fontSize: '.7rem', color: 'white'}} height={100} className="text-white"/>
                 <YAxis type="number" domain={[0,20]}/>
                 <Tooltip labelFormatter={(label) => moment(label).format("YYYY-MM-DD HH:mm:ss")}/>
+                <Legend />
               </LineChart>
             </ResponsiveContainer>
             {data.data_0002DO_1.length==0?
@@ -199,11 +196,12 @@ function App() {
             <p>TEMP</p>
             <ResponsiveContainer width="100%" height={400}>
               <LineChart width={1000} height={400} data={data.data_0002TEMP} className="mx-auto">
-                <Line type="monotone" dataKey="value"  stroke="#8884d8"/>
+                <Line type="monotone" dataKey="value" name="TEMP" stroke="#ffc658" dot={false}/>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" type="number" tickFormatter={(unixTimestamp) => moment(unixTimestamp).format("YYYY-MM-DD HH:mm:ss")} domain={['auto', 'auto']} scale="time"  allowDuplicatedCategory={false} angle={-45} textAnchor="end" tick={{ fontSize: '.7rem', color: 'white'}} height={100} className="text-white"/>
                 <YAxis type="number" domain={[0,20]}/>
                 <Tooltip labelFormatter={(label) => moment(label).format("YYYY-MM-DD HH:mm:ss")}/>
+                <Legend />
               </LineChart>
             </ResponsiveContainer>
             {data.data_0002TEMP.length==0?
@@ -223,12 +221,13 @@ function App() {
             <p>DO</p>
             <ResponsiveContainer width="100%" height={400}>
               <LineChart width={1000} height={400} className="mx-auto">
-                <Line type="monotone" dataKey="value" data={data.data_0003DO_1} stroke="#8884d8"/>
-                <Line type="monotone" dataKey="value" data={data.data_0003DO_2} stroke="#82ca9d"/>
+                <Line type="monotone" dataKey="value" data={data.data_0003DO_1} name="DO1" stroke="#8884d8" dot={false}/>
+                <Line type="monotone" dataKey="value" data={data.data_0003DO_2} name="DO2" stroke="#82ca9d" dot={false}/>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" type="number" tickFormatter={(unixTimestamp) => moment(unixTimestamp).format("YYYY-MM-DD HH:mm:ss")} domain={['auto', 'auto']} scale="time"  allowDuplicatedCategory={false} angle={-45} textAnchor="end" tick={{ fontSize: '.7rem', color: 'white'}} height={100} className="text-white"/>
                 <YAxis type="number" domain={[0,20]}/>
                 <Tooltip labelFormatter={(label) => moment(label).format("YYYY-MM-DD HH:mm:ss")}/>
+                <Legend />
               </LineChart>
             </ResponsiveContainer>
             {data.data_0003DO_1.length==0?
@@ -250,25 +249,29 @@ function App() {
             <p>TEMP</p>
             <ResponsiveContainer width="100%" height={400}>
               <LineChart width={1000} height={400} data={data.data_0003TEMP} className="mx-auto">
-                <Line type="monotone" dataKey="value"  stroke="#8884d8"/>
+                <Line type="monotone" dataKey="value" name="TEMP" stroke="#ffc658" dot={false}/>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" type="number" tickFormatter={(unixTimestamp) => moment(unixTimestamp).format("YYYY-MM-DD HH:mm:ss")} domain={['auto', 'auto']} scale="time"  allowDuplicatedCategory={false} angle={-45} textAnchor="end" tick={{ fontSize: '.7rem', color: 'white'}} height={100} className="text-white"/>
                 <YAxis type="number" domain={[0,20]}/>
                 <Tooltip labelFormatter={(label) => moment(label).format("YYYY-MM-DD HH:mm:ss")}/>
+                <Legend />
               </LineChart>
             </ResponsiveContainer>
             {data.data_0003TEMP.length==0?
             <div class="alert alert-error shadow-lg">
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>No data found</span>
+                <span>No data found (TEMP)</span>
               </div>
             </div>
             :<></>}
           </div>
         </div>
-
       </div>
+
+      <footer className="footer p-10 bg-neutral text-neutral-content mt-5 text-center content-center">
+        <a href="https://github.com/kazuyahirotsu/LoRa" target="_blank" className='mx-auto'>github repo</a>
+      </footer>
     </div>
   );
 }
